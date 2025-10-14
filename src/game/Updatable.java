dd0519321016a7d79a3f10457cfc0cac669cda57
Max Sorokin <156485161+max-sorokin-ft@users.noreply.github.com>
2025-10-14 16:47:54 -0400

    Update Updatable.java

--- src/game/Updatable.java
+++ src/game/Updatable.java
@@ -1,7 +1,16 @@
 package game;
 
+/**
+ * This interface is for game objects that need to be updated during each frame.
+ * Classes that implement this interface should define behavior for their update logic of things such as
+ * movement, animation, or state changes.
+ */
 public interface Updatable {
 
-	public void update();
-	
+    /**
+     * Updates the object's state for the current frame.
+     * It is called once per game loop iteration.
+     */
+    public void update();
+    
 }
