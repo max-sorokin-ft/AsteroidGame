4c9d1c8aef0a7ae73e3c27497d60e9f2f51e7514
Max Sorokin <156485161+max-sorokin-ft@users.noreply.github.com>
2025-10-14 16:47:04 -0400

    Update Drawable.java

--- src/game/Drawable.java
+++ src/game/Drawable.java
@@ -2,8 +2,18 @@ package game;
 
 import java.awt.Graphics;
 
+/**
+ * This interface is for game objects that can be drawn to the screen.
+ * Implementing classes should define their visual representation
+ * using the provided Graphics object.
+ */
 public interface Drawable {
 
-	void draw(Graphics brush);
-	
+    /**
+     * Draws the object to the screen.
+     * 
+     * @param brush the Graphics object used for drawing
+     */
+    void draw(Graphics brush);
+    
 }
