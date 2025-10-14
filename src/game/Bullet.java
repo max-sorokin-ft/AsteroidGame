028c83640b57d7cbf33b7b2f94fb8b62e1c0c8ba
Max Sorokin <156485161+max-sorokin-ft@users.noreply.github.com>
2025-10-14 16:46:37 -0400

    Update Bullet.java

--- src/game/Bullet.java
+++ src/game/Bullet.java
@@ -3,6 +3,10 @@ package game;
 import java.awt.Color;
 import java.awt.Graphics;
 
+/**
+ * This class represents a projectile/bullet fired by the player's ship.
+ * Bullets travel in a straight line and disappear after a set time not wrapping screen like other elements.
+ */
 public class Bullet implements Updatable, Drawable {
     
     private Point position;
@@ -11,6 +15,13 @@ public class Bullet implements Updatable, Drawable {
     private int lifespan;
     private static final int MAX_LIFESPAN = 60;
     
+    /**
+     * Constructs a new Bullet at the specified position.
+     * 
+     * @param startPosition the initial spawn position
+     * @param angle the direction of travel in degrees
+     * @param velocity the speed in pixels per frame
+     */
     public Bullet(Point startPosition, double angle, double velocity) {
         this.position = startPosition.clone();
         this.angle = angle;
@@ -18,23 +29,41 @@ public class Bullet implements Updatable, Drawable {
         this.lifespan = 0;
     }
     
+    /**
+     * Updates the bullet's position each frame.
+     * Moves the bullet and increments its lifespan.
+     */
     @Override
     public void update() {
         position = Utilities.updatePosition(position, angle, velocity);
-        // Bullets disappear off screen, rather than wrapping like ship.
         lifespan++;
     }
     
+    /**
+     * Draws the bullet to be a white circle.
+     * 
+     * @param brush the Graphics object used for drawing
+     */
     @Override
     public void draw(Graphics brush) {
         brush.setColor(Color.white);
         brush.fillOval((int)position.getX() - 2, (int)position.getY() - 2, 4, 4);
     }
     
+    /**
+     * Checks if the bullet should be removed from the game.
+     * 
+     * @return true if the bullet has exceeded its lifespan
+     */
     public boolean isDead() {
         return lifespan > MAX_LIFESPAN;
     }
     
+    /**
+     * Returns the current position for collision detection.
+     * 
+     * @return the current Point position
+     */
     public Point getPosition() {
         return position;
     }
